40dc904a2fc5dcef4c1f4d6e1cb9baaf33c82eb1
Marek Praski <marekpraski@wp.pl>
2020-01-08 15:15:50 +0100

    poprawiony błąd podczas kasowania maili z serwera; poprawiony błąd, że gdy na serwerze była tylko jedna wiadomość, to jej nie ładował

---
+++ EmailNotifier/EmailService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailNotifier
+{
+    public class EmailService : IEmailService
+    {
+        public virtual void DeleteEmails(IList<IEmailMessage> emailsToDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual LinkedList<IEmailMessage> ReceiveAndDelete(IEmailMessage lastEmail, IList<IEmailMessage> emailsToDelete = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual LinkedList<IEmailMessage> ReceiveEmails(int numberOfMessagesToReceive)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual LinkedList<IEmailMessage> ReceiveEmails(IEmailMessage lastEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void SendEmails(IList<IEmailMessage> emailMessages)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected IEmailMessage getOldestEmail(IList<IEmailMessage> emailsToDelete)
+        {
+            DateTime oldestDate = emailsToDelete[0].messageDateTime;
+            int messageIndex = 0;
+
+            for( int i = 1; i< emailsToDelete.Count; i++)
+            {
+                if(oldestDate > emailsToDelete[i].messageDateTime)      //im wiadomość młodsza, tym data większa
+                {
+                    oldestDate = emailsToDelete[i].messageDateTime;
+                    messageIndex = i;
+                }
+            }
+            return emailsToDelete[messageIndex];
+        }
+
+
+        protected Dictionary<string, IEmailMessage> constructEmailToDeleteDict(IList<IEmailMessage> emailsToDelete)
+        {
+            Dictionary<string, IEmailMessage> emailsToDeleteDict = new Dictionary<string, IEmailMessage>();
+
+            string emailId;
+
+            for (int i = 0; i < emailsToDelete.Count; i++)
+            {
+                emailId = emailsToDelete[i].messageId;
+                emailsToDeleteDict.Add(emailId, emailsToDelete[i]);
+            }
+
+            return emailsToDeleteDict;
+        }
+    }
+}
